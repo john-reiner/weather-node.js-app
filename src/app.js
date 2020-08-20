a346fc4849173de517a474f5534938269c17438a
john-reiner <johnreiner1990@gmail.com>
2020-08-20 12:48:26 -0400

    set up handlebars and template directory configured express to look for views in the templates directory instead of views

--- src/app.js
+++ src/app.js
@@ -1,11 +1,41 @@
 const express = require('express')
 const path = require('path')
+const { request } = require('express')
 
 const app = express()
 
+// Paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
 
+
+// setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+// setup static directory to serve
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'John Reiner'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: "John Reiner"
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        message: 'This pages is designed to help you out.'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
