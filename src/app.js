9293a488681d58e9d9b99329bbc0d2092cd5e1fa
john-reiner <johnreiner1990@gmail.com>
2020-08-20 13:18:54 -0400

    created partials for header and footer

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
 const express = require('express')
 const path = require('path')
 const { request } = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 // Paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 
 // setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // setup static directory to serve
 app.use(express.static(publicDirectoryPath))
@@ -26,13 +29,14 @@ app.get('', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: "John Reiner"
+        name: "John Andrew Reiner"
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help',
+        name: 'John Reiner',
         message: 'This pages is designed to help you out.'
     })
 })
